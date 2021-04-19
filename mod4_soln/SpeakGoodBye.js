ab1712bb8ada78560c74ad26948674c96ebe569c
RefShah <35521737+RefShah@users.noreply.github.com>
2021-04-19 08:00:17 +0800

    Update SpeakGoodBye.js

--- mod4_soln/SpeakGoodBye.js
+++ mod4_soln/SpeakGoodBye.js
@@ -15,11 +15,11 @@
   // STEP 8: Rewrite the 'speak' function such that it is attached to the
   // byeSpeaker object instead of being a standalone function.
   // See Lecture 52, part 2
-  byeSpeaker.speak = function (name) {
+  window.speak = function (name) {
     console.log(speakWord + " " + name);
   }
 
-  window.byeSpeaker = byeSpeaker;
+  window.byeSpeaker = speak;
 
 })(window);
 
